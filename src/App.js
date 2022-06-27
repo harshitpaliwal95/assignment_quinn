642dfc0f3467f0445a9ac9a3a453b18a11dafb7d
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-06-27 22:08:26 +0530

    CHORE: basic file

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="grid-two">
+        <div className="copy-comp flex-comp">
+          <h3>Text</h3>
+          <button className="btn btn-outline">Button</button>
+        </div>
+        <div className="canvas"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
